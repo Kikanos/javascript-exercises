b788c23fa2789470cf74fd605e7a2cfc605784f2
Kikanos <r_baouchi@hotmail.com>
2024-03-20 11:07:36 +0100

    update

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...deletedElem) {
+  arr.forEach(() => {
+    deletedElem.forEach((elem) => {
+      index = arr.indexOf(elem);
+      if (index > -1) {
+        arr.splice(index, 1);
+      }
+    });
+  });
+  return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let count = 0;
 
+  if (typeof a !== "number" || typeof b !== "number" || a * b < 0) {
+    return "ERROR";
+  } else {
+    const max = Math.max(a, b);
+    const min = Math.min(a, b);
+    for (let i = min; i <= max; i++) {
+      count += i;
+    }
+    return count;
+  }
 };
 
 // Do not edit below this line
